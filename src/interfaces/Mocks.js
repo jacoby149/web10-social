b9c057899c218bf9d2baf6bf0fe8de17293c83db
jacoby149 <jackstudent149@gmail.com>
2022-12-08 11:22:42 -0500

    clean mock up of web10 social

--- src/interfaces/Mocks.js
+++ src/interfaces/Mocks.js
@@ -1,32 +1,46 @@
-import lillyIco from "../components/images/avatar1.svg"
-import joeyIco from "../components/images/avatar1.svg"
-import emilyIco from "../components/images/avatar1.svg"
-import kaiIco from "../components/images/avatar1.svg"
+import lillyIco from "../components/images/avatar9.png"
+import joeyIco from "../components/images/avatar6.png"
+import emilyIco from "../components/images/avatar7.png"
+import AaronIco from "../components/images/avatar4.png"
+import ShadiyaIco from "../components/images/avatar10.png"
+import BrianIco from "../components/images/avatar2.png"
 
 const mockContacts = [
     {
         name: "Emily",
         lastSenderName: "Emily",
-        info: "Yes I can do it for you",
+        message: "Yes I can do it for you",
         pic: emilyIco
     },
     {
         name: "Lilly",
         lastSenderName: "Lilly",
-        info: "what time are you thinking?",
+        message: "what time are you thinking?",
         pic: lillyIco
     },
     {
         name: "Joey",
         lastSenderName: "Joey",
-        info: "OK. sounds good, thanks!",
+        message: "OK. sounds good, thanks!",
         pic: joeyIco
     },
     {
-        name: "Kai",
-        lastSenderName: "Kai",
-        info: "hahaha :)",
-        pic: kaiIco
+        name: "Aaron",
+        lastSenderName: "Aaron",
+        message: "hahaha :)",
+        pic: AaronIco
+    },
+    {
+        name: "Shadiya",
+        lastSenderName: "Shadiya",
+        message: "can't wait!",
+        pic: ShadiyaIco
+    },
+    {
+        name: "Brian",
+        lastSenderName: "Brian",
+        message: "Let's talk later.",
+        pic: BrianIco
     },
 ]
 
