e9cdc072b8e34346741199fd0e1a51f48956ef02
jacoby149 <jackstudent149@gmail.com>
2022-12-08 14:38:56 -0500

    fixed the onclick garbage

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -2,40 +2,25 @@
 // active status, etc.
 
 import { R, C } from 'rectangles-npm'
-import {Conversation, Avatar, ConversationList } from '@chatscope/chat-ui-kit-react'
+import { Conversation, Avatar, ConversationList } from '@chatscope/chat-ui-kit-react'
 import styles from '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import './bulma/css/bulma.min.css';
 import './Contacts.css';
 import TopBar
- from './shared/TopBar';
- import Sidebar from './shared/SideBar';
-// an individual contact.
-function Contact({ name, lastSenderName, pic, message, theme }) {
-  return (
-    <a href="/?mock=chat">
-      <Conversation name={name} className={`contacts ${theme}`} lastSenderName={lastSenderName} info={message}>
-        <Avatar src={pic} name={name} />
-      </Conversation>
-    </a>
-
-  )
-}
+  from './shared/TopBar';
+import Sidebar from './shared/SideBar';
 
 function Contacts({ I }) {
-  const contactItems = I.contacts.map((contact,index) => {
-      return <Contact 
-        key={index}
-        name={contact.name}
-        pic = {contact.pic}
-        lastSenderName = {contact.lastSenderName}
-        message = {contact.message}
-        theme={I.theme}
-      />  
-    });
-  
+  const contactItems = I.contacts.map((contact, index) => {
+    return <Conversation key={index} onClick={()=>I.setMode("chat")} name={contact.name} className={`contacts ${I.theme}`} lastSenderName={contact.lastSenderName} info={contact.message}>
+      <Avatar src={contact.pic} name={contact.name} />
+    </Conversation>
+
+  });
+
   return (
     <R root t bt bb br bl onClick={I.toggleTheme} theme={I.theme}>
-      <TopBar I ={I}/>
+      <TopBar I={I} />
       <R l tel>
         <Sidebar I={I}></Sidebar>
         <R t tel>
