c4c0f309bf2734f0974182656f6ad9d1aab36ab0
jacoby149 <jackstudent149@gmail.com>
2022-12-09 17:53:59 -0500

    good idea

--- src/components/Feed/CreatePost.js
+++ src/components/Feed/CreatePost.js
@@ -9,12 +9,17 @@ function CreatePost() {
                     <p class="card-header-title">
                         Make a Post
                     </p>
-                        <button style={{margin:"10px"}} className={"button is-primary is-small"}>Create Post</button>
+                    <button style={{ margin: "10px", width: "130px" }} className={"button is-primary is-small"}>Create Post</button>
                 </header>
                 <div class="card-content">
                     <div class="content">
                         <div class="control">
                             <textarea class="textarea" placeholder="What is on your mind??"></textarea>
+                        </div>
+                        <div>
+                        <img style={{ marginTop: "5px", marginRight: "5px" }} src="https://bulma.io/images/placeholders/128x128.png" />
+                            <img style={{ marginTop: "5px", marginRight: "5px" }} src="https://bulma.io/images/placeholders/128x128.png" />
+                            <video style={{ height:"128px",marginTop: "5px", marginRight: "5px" }} controls />
                         </div>
                     </div>
                 </div>
