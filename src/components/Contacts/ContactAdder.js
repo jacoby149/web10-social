4603d8c3de27a4851290219c3c8369cdb6fc19a1
jacoby149 <jackstudent149@gmail.com>
2022-12-10 10:09:59 -0500

    cleaned it up a little better

--- src/components/Contacts/ContactAdder.js
+++ src/components/Contacts/ContactAdder.js
@@ -2,7 +2,8 @@ import { Conversation, Avatar, ConversationList } from '@chatscope/chat-ui-kit-r
 import contactIco from "../../assets/images/Contact.png"
 
 function ContactAdder({I}) {
-    const toAdd = I.search?`add ${I.search}`:"add a contact";
+    const user = I.search.includes("/")?I.search:`api.web10.app/${I.search}`;
+    const toAdd = I.search?`add ${user}`:"add a contact";
     const label = I.search?"on web10 social?":"To Add A Contact";
     const status = I.search?"no. add anyway!":"Type In The Search Bar!"
     return (
