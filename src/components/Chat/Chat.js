9c79b31354dc1a4b4b0006e1f32c638e25967345
jacoby149 <jackstudent149@gmail.com>
2022-12-09 21:11:29 -0500

    working perfectly

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,15 +1,37 @@
 // see private messages between you and a friend / group.
 
 import { R, C } from 'rectangles-npm'
-import { Search, ChatContainer, ConversationHeader, Avatar, VoiceCallButton, VideoCallButton, InfoButton, MessageList, TypingIndicator, MessageSeparator, Message, MessageInput } from '@chatscope/chat-ui-kit-react'
+import { Search, ChatContainer, ConversationHeader, Avatar, VoiceCallButton, VideoCallButton, InfoButton, TypingIndicator, MessageInput, MessageList, Message,MessageSeparator } from '@chatscope/chat-ui-kit-react';
 import styles from '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import emilyIco from "../../assets/images/avatar1.svg"
 import TopBar from '../shared/TopBar';
 import SideBar from '../shared/SideBar'
 import { RawIcon } from '../shared/Icon'
-
+import { mockChat } from '../../interfaces/Mocks';
 
 function Chat({ I }) {
+    var messageItems = [];
+    for (const [index, model] of mockChat.entries()) {
+        const present = new Date(model["sentTime"]);
+        if (index == 0) {
+            messageItems.push(
+                <MessageSeparator key={`s${index}`} content={present.toDateString()} />
+            )
+        }
+        else if (index > 1) {
+            const past = new Date(mockChat[index - 1]["sentTime"]);
+            if (present.getDay() - past.getDay() > 0) {
+                messageItems.push(
+                    <MessageSeparator key={`s${index}`} content={present.toDateString()} />
+                )
+            }
+        }
+        messageItems.push(
+            <Message key={index} model={model} />
+        )
+    }
+
+
     return (
         <R root t bt bb br bl theme={I.theme}>
             <TopBar I={I} />
@@ -32,201 +54,7 @@ function Chat({ I }) {
 
                             <MessageList className={I.theme} typingIndicator={<TypingIndicator content="Emily is typing" />}>
 
-                                <MessageSeparator content="Saturday, 30 November 2019" />
-
-                                <Message model={{
-                                    message: "Hello my friend",
-                                    sentTime: "15 mins ago",
-                                    sender: "Emily",
-                                    direction: "incoming",
-                                    position: "single"
-                                }}>
-                                    <Avatar src={emilyIco} name={"Emily"} />
-                                </Message>
-                                <Message model={{
-                                    message: "Hello my friend",
-                                    sentTime: "15 mins ago",
-                                    sender: "me",
-                                    direction: "outgoing",
-                                    position: "single"
-                                }} />
-                                <Message model={{
-                                    message: "Hello my friend",
-                                    sentTime: "15 mins ago",
-                                    sender: "Emily",
-                                    direction: "incoming",
-                                    position: "first"
-                                }} avatarSpacer >
-
-                                    <Message.Footer sender="Emily" sentTime="just now" />
-                                </Message>
-                                <Message model={{
-                                    message: "Hello my friend",
-                                    sentTime: "15 mins ago",
-                                    sender: "Emily",
-                                    direction: "incoming",
-                                    position: "normal"
-                                }} avatarSpacer />
-                                <Message model={{
-                                    message: "Hello my friend",
-                                    sentTime: "15 mins ago",
-                                    sender: "Emily",
-                                    direction: "incoming",
-                                    position: "normal"
-                                }} avatarSpacer />
-                                <Message model={{
-                                    message: "Hello my friend",
-                                    sentTime: "15 mins ago",
-                                    sender: "Emily",
-                                    direction: "incoming",
-                                    position: "last"
-                                }}>
-                                    <Avatar src={emilyIco} name={"Emily"} />
-                                </Message>
-                                <Message model={{
-                                    message: "Hello my friend",
-                                    sentTime: "15 mins ago",
-                                    direction: "outgoing",
-                                    position: "first"
-                                }} >
-                                </Message>
-                                <Message model={{
-                                    message: "Hello my friend",
-                                    sentTime: "15 mins ago",
-                                    direction: "outgoing",
-                                    position: "normal"
-                                }} />
-                                <Message model={{
-                                    message: "Hello my friend",
-                                    sentTime: "15 mins ago",
-                                    direction: "outgoing",
-                                    position: "normal"
-                                }} />
-                                <Message model={{
-                                    message: "Hello my friend",
-                                    sentTime: "15 mins ago",
-                                    direction: "outgoing",
-                                    position: "last"
-                                }} />
-
-                                <Message model={{
-                                    message: "Hello my friend",
-                                    sentTime: "15 mins ago",
-                                    sender: "Emily",
-                                    direction: "incoming",
-                                    position: "first"
-                                }} avatarSpacer />
-                                <Message model={{
-                                    message: "Hello my friend",
-                                    sentTime: "15 mins ago",
-                                    sender: "Emily",
-                                    direction: "incoming",
-                                    position: "last"
-                                }}>
-                                    <Avatar src={emilyIco} name={"Emily"} />
-                                </Message>
-
-                                <MessageSeparator content="Saturday, 31 November 2019" />
-
-                                <Message model={{
-                                    message: "Hello my friend",
-                                    sentTime: "15 mins ago",
-                                    sender: "Emily",
-                                    direction: "incoming",
-                                    position: "single"
-                                }}>
-                                    <Avatar src={emilyIco} name={"Emily"} />
-                                </Message>
-                                <Message model={{
-                                    message: "Hello my friend",
-                                    sentTime: "15 mins ago",
-                                    sender: "me",
-                                    direction: "outgoing",
-                                    position: "single"
-                                }} >
-                                    <Message.Footer sentTime="3 mins ago" />
-                                </Message>
-                                <Message model={{
-                                    message: "Hello my friend",
-                                    sentTime: "15 mins ago",
-                                    sender: "Emily",
-                                    direction: "incoming",
-                                    position: "first"
-                                }} avatarSpacer />
-                                <Message model={{
-                                    message: "Hello my friend",
-                                    sentTime: "15 mins ago",
-                                    sender: "Emily",
-                                    direction: "incoming",
-                                    position: "normal"
-                                }} avatarSpacer />
-                                <Message model={{
-                                    message: "Hello my friend",
-                                    sentTime: "15 mins ago",
-                                    sender: "Emily",
-                                    direction: "incoming",
-                                    position: "normal"
-                                }} avatarSpacer />
-                                <Message model={{
-                                    message: "Hello my friend",
-                                    sentTime: "15 mins ago",
-                                    sender: "Emily",
-                                    direction: "incoming",
-                                    position: "last"
-                                }}>
-                                    <Avatar src={emilyIco} name={"Emily"} />
-                                </Message>
-                                <Message model={{
-                                    message: "Hello my friend",
-                                    sentTime: "15 mins ago",
-                                    direction: "outgoing",
-                                    position: "first"
-                                }} />
-                                <Message model={{
-                                    message: "Hello my friend",
-                                    sentTime: "15 mins ago",
-                                    direction: "outgoing",
-                                    position: "normal"
-                                }} />
-                                <Message model={{
-                                    message: "Hello my friend",
-                                    sentTime: "15 mins ago",
-                                    direction: "outgoing",
-                                    position: "normal"
-                                }} >
-                                    <Message.Footer sender="Emily" sentTime="6:57pm" />
-
-                                </Message>
-                                <Message model={{
-                                    message: "FOLLOW THE BELOW INSTRUCTIONS",
-                                    sentTime: "15 mins ago",
-                                    direction: "outgoing",
-                                    position: "last"
-                                }} >
-                                    <Message.Footer sender="Emily" sentTime="6:57pm" />
-                                </Message>
-
-                                <Message model={{
-                                    message: "CLICK THE INFO ICON",
-                                    sentTime: "15 mins ago",
-                                    sender: "Emily",
-                                    direction: "incoming",
-                                    position: "first"
-                                }} avatarSpacer >
-                                    <Message.Footer sentTime="6:57pm" />
-
-                                </Message>
-                                <Message className={I.theme} model={{
-                                    message: "INFO ICON GOES TO BIO",
-                                    sentTime: "12 mins ago",
-                                    sender: "Emily",
-                                    direction: "incoming",
-                                    position: "single"
-                                }} >
-                                    <Avatar src={emilyIco} name={"Emily"} />
-                                    <Message.Header sender="Emily"></Message.Header>
-                                    <Message.Footer sentTime="2 mins ago" />
-                                </Message>
+                                {messageItems}
 
                             </MessageList>
                             <MessageInput className={I.theme} placeholder="Type message here" />
--- src/interfaces/Mocks.js
+++ src/interfaces/Mocks.js
@@ -44,20 +44,180 @@ const mockContacts = [
     },
 ]
 
+
+const mockDates = [
+    "Fri Dec 04 2022 14:42:26 GMT-0500 (Eastern Standard Time)",
+    "Fri Dec 04 2022 16:49:26 GMT-0500 (Eastern Standard Time)",
+    "Fri Dec 04 2022 15:17:26 GMT-0500 (Eastern Standard Time)",
+    "Fri Dec 04 2022 17:27:26 GMT-0500 (Eastern Standard Time)",
+    "Fri Dec 04 2022 18:25:26 GMT-0500 (Eastern Standard Time)",
+    "Fri Dec 05 2022 11:53:26 GMT-0500 (Eastern Standard Time)",
+    "Fri Dec 05 2022 12:57:26 GMT-0500 (Eastern Standard Time)",
+    "Fri Dec 05 2022 13:59:26 GMT-0500 (Eastern Standard Time)",
+    "Fri Dec 05 2022 14:42:26 GMT-0500 (Eastern Standard Time)",
+    "Fri Dec 05 2022 15:17:26 GMT-0500 (Eastern Standard Time)"
+]
+
 const mockChat = [
     {
         message: "Hello my friend",
-        sentTime: "15 mins ago",
+        sentTime: mockDates[0],
         sender: "Emily",
         direction: "incoming",
         position: "single"
+    },
+    {
+        message: "Hello my friend",
+        sentTime: mockDates[1],
+        sender: "me",
+        direction: "outgoing",
+        position: "single"
+    },
+    {
+        message: "Hello my friend",
+        sentTime: mockDates[2],
+        sender: "Emily",
+        direction: "incoming",
+        position: "first"
+    },
+    {
+        message: "Hello my friend",
+        sentTime: mockDates[2],
+        sender: "Emily",
+        direction: "incoming",
+        position: "normal"
+    },
+    {
+        message: "Hello my friend",
+        sentTime: mockDates[2],
+        sender: "Emily",
+        direction: "incoming",
+        position: "normal"
+    },
+    {
+        message: "Hello my friend",
+        sentTime: mockDates[2],
+        sender: "Emily",
+        direction: "incoming",
+        position: "last"
+    },
+    {
+        message: "Hello my friend",
+        sentTime: mockDates[3],
+        direction: "outgoing",
+        position: "first"
+    },
+    {
+        message: "Hello my friend",
+        sentTime: mockDates[3],
+        direction: "outgoing",
+        position: "normal"
+    },
+    {
+        message: "Hello my friend",
+        sentTime: mockDates[3],
+        direction: "outgoing",
+        position: "normal"
+    },
+    {
+        message: "Hello my friend",
+        sentTime: mockDates[3],
+        direction: "outgoing",
+        position: "last"
+    },
+    {
+        message: "Hello my friend",
+        sentTime: mockDates[4],
+        sender: "Emily",
+        direction: "incoming",
+        position: "first"
+    },
+    {
+        message: "Hello my friend",
+        sentTime: mockDates[4],
+        sender: "Emily",
+        direction: "incoming",
+        position: "last"
     }, {
         message: "Hello my friend",
-        sentTime: "15 mins ago",
+        sentTime: mockDates[5],
+        sender: "Emily",
+        direction: "incoming",
+        position: "single"
+    },
+    {
+        message: "Hello my friend",
+        sentTime: mockDates[6],
+        sender: "me",
+        direction: "outgoing",
+        position: "single"
+    },
+    {
+        message: "Hello my friend",
+        sentTime: mockDates[7],
+        sender: "Emily",
+        direction: "incoming",
+        position: "first"
+    },
+    {
+        message: "Hello my friend",
+        sentTime: mockDates[7],
+        sender: "Emily",
+        direction: "incoming",
+        position: "normal"
+    },
+    {
+        message: "Hello my friend",
+        sentTime: mockDates[7],
+        sender: "Emily",
+        direction: "incoming",
+        position: "normal"
+    },
+    {
+        message: "Hello my friend",
+        sentTime: mockDates[7],
+        sender: "Emily",
+        direction: "incoming",
+        position: "last"
+    },
+    {
+        message: "Hello my friend",
+        sentTime: mockDates[8],
+        direction: "outgoing",
+        position: "first"
+    },
+    {
+        message: "Hello my friend",
+        sentTime: mockDates[8],
+        direction: "outgoing",
+        position: "normal"
+    },
+    {
+        message: "Hello my friend",
+        sentTime: mockDates[8],
+        direction: "outgoing",
+        position: "normal"
+    },
+    {
+        message: "FOLLOW THE BELOW INSTRUCTIONS",
+        sentTime: mockDates[8],
+        direction: "outgoing",
+        position: "last"
+    },
+    {
+        message: "CLICK THE INFO ICON",
+        sentTime: mockDates[9],
+        sender: "Emily",
+        direction: "incoming",
+        position: "first"
+    },
+    {
+        message: "INFO ICON GOES TO BIO",
+        sentTime: mockDates[9],
         sender: "Emily",
         direction: "incoming",
         position: "single"
     }
 ]
 
-export { mockContacts }
+export { mockContacts, mockChat }
