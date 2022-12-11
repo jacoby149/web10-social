bde182d6b2d3f9283627e757b2b503550b001ca3
jacoby149 <jackstudent149@gmail.com>
2022-12-11 12:22:18 -0500

    edit your identity

--- src/components/Bio/Identity.js
+++ src/components/Bio/Identity.js
@@ -4,23 +4,35 @@ import styles from '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import emilyIco from "../../assets/images/avatar1.svg"
 
 
-function Identity({I}) {
+function Identity({ I }) {
     return (
         <R t theme={I.theme}>
             <C t ha="center" va="center">
                 <Avatar style={{ margin: "20px" }} size={"lg"} src={emilyIco} name="Emily" />
+                {
+                    I.mode === "my-bio-edit" ?
+                        <button class="button is-warning is-small">upload photo</button> : ""
+                }
             </C>
             <R t ns s={"24px"} h>
                 <div className="columns is-centered">
                     <div className="column has-text-centered is-4">
-                        <i>name : Jacob Hoffman</i>
+                        {
+                            I.mode === "my-bio-edit" ?
+                                <i>[<input size="22" style={{ color: "gold" }} defaultValue="Jacob Hoffman"></input>]</i> :
+                                <i>name : Jacob Hoffman</i>
+                        }
                     </div>
                 </div>
             </R>
             <R t ns s={"30px"} h>
                 <div className="columns is-centered">
                     <div className="column has-text-centered is-4">
-                        <i>bio : i am such a zoomer kid . . .</i>
+                        {
+                            I.mode === "my-bio-edit" ?
+                                <i>[<input size="32" style={{ color: "gold" }} defaultValue={"i am such a zoomer kid . . ."}></input>]</i> :
+                                <i>bio : i am such a zoomer kid . . .</i>
+                        }
                     </div>
                 </div>
             </R>
