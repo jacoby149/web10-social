823f8b9f5d1e76d7981b893cac9d8c84b06f2aff
jacoby149 <jackstudent149@gmail.com>
2022-12-12 10:16:21 -0500

    good point to commit at

--- src/mocks/MockWall.js
+++ src/mocks/MockWall.js
@@ -1,23 +1,6 @@
-const mockWall = [{
-    "name": "Jacob Hoffman",
-    "profile": "me.png",
-    "time": "10:04:59 AM",
-    "images": ["lala.png", "haha.png"],
-    "files": ["hehe.pdf", "lelel.pdf"],
-    "text": "I wanted to share! I had this idea for a new internet platform, that I call web10. It gives every user on the internet a domain name for them to control their data. One more time for posterity, it is called web10, it is totally awesome and the future of the internet.",
-    "email": "api.web10.app/jacoby149",
-    "subject": "heeheehee"
-},
-{
-    "name": "Jacob Hoffman",
-    "profile": "me.png",
-    "time": "10:04:59 AM",
-    "images": ["lala.png", "haha.png"],
-    "files": ["hehe.pdf", "lelel.pdf"],
-    "text": "This is <i>jacob</i>, making my <b>first</b> post haha.",
-    "email": "api.web10.app/jacoby149",
-    "subject": "hoohoohoo"
-}
-]
+import mockFeed from "./MockFeed";
+
+// only take the posts that have my name for the wall.
+const mockWall = mockFeed.filter((post)=>post.name==="Jacob Hoffman")
 
 export default mockWall;
