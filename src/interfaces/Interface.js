1bce9a669e45f1335e9bfe63b76677f9f8914ae8
jacoby149 <jackstudent149@gmail.com>
2022-12-13 10:20:36 -0500

    make a mock run search

--- src/interfaces/Interface.js
+++ src/interfaces/Interface.js
@@ -12,97 +12,126 @@ import mockBulletin from '../mocks/MockBulletin';
 function useInterface() {
     const I = {};
     [I.theme, I.setTheme] = React.useState("dark");
-    [I.menuCollapsed,I.setMenuCollapsed] = React.useState(true);
-    [I.mode,I._setMode] = React.useState("login");
-    [I.search,I.setSearch] = React.useState("");
+    [I.menuCollapsed, I.setMenuCollapsed] = React.useState(true);
+    [I.mode, I._setMode] = React.useState("login");
+    [I.search, I.setSearch] = React.useState("");
 
     [I.contacts, I.setContacts] = React.useState(mockContacts);
-    [I.currentContact,I.setCurrentContact] = React.useState(I.contacts[0]);
-    
-    [I.draftPost,I.setDraftPost] = React.useState(mockDraftPost);
-    [I.feedPosts,I.setFeedPosts] = React.useState(mockFeed);
-    [I.wallPosts,I.setWallPosts] = React.useState(mockWall);
-    
-    [I.bulletin,I.setBulletin] = React.useState(mockBulletin);
+    [I.currentContact, I.setCurrentContact] = React.useState(I.contacts[0]);
 
-    [I.identity,I.setIdentity] = React.useState(mockIdentity);
-    [I.draftIdentity,I.setDraftIdentity] = React.useState(I.identity);
+    [I.draftPost, I.setDraftPost] = React.useState(mockDraftPost);
+    [I.feedPosts, I.setFeedPosts] = React.useState(mockFeed);
+    [I.wallPosts, I.setWallPosts] = React.useState(mockWall);
 
-    [I.currentMessages,I.setCurrentMessages] = React.useState(mockChat);
-    [I.selectedMessages,I.setSelectedMessages] = React.useState([]);
+    [I.bulletin, I.setBulletin] = React.useState(mockBulletin);
 
-    I.help = function(){
+    [I.identity, I.setIdentity] = React.useState(mockIdentity);
+    [I.draftIdentity, I.setDraftIdentity] = React.useState(I.identity);
+
+    [I.currentMessages, I.setCurrentMessages] = React.useState(mockChat);
+    [I.selectedMessages, I.setSelectedMessages] = React.useState([]);
+
+    I.help = function () {
         console.log("the real web10 interface!")
     }
 
-    I.getPosts = function(web10){
-        return I.feedPosts.filter((p)=>p.web10===web10)
+    I.runSearch = function (query) {
+        function chatFilter(m){
+            m.message.toLowerCase().includes(query.toLowerCase());
+        }
+        function contactFilter(c){
+            c.name.toLowerCase().includes(query.toLowerCase());
+        }
+        function postFilter(p){
+            p.html.toLowerCase().includes(query.toLowerCase());
+        }
+
+        switch (I.mode) {
+
+            // chat like pages, that have private messages
+            case "chat": I.setCurrentMessages(mockChat.filter(chatFilter));
+            case "chat-edit": I.setCurrentMessages(mockChat.filter(chatFilter));
+
+            // bio like pages, that have a social-identity and social-bulletin 
+            case "bio": I.setWallPosts(mockWall.filter(postFilter));
+            case "my-bio": I.setWallPosts(mockWall.filter(postFilter));
+            case "bio-edit": I.setWallPosts(mockWall.filter(postFilter));
+            case "bulletin-edit": I.setWallPosts(mockWall.filter(postFilter));
+
+            // feed like pages, that consist of posts with images,vids,audio, and html
+            case "feed": I.setFeedPosts(mockWall.filter(postFilter));
+        }
+        I.setSearch(query)
     }
-    I.getContact = function(web10){
+
+    I.getPosts = function (web10) {
+        return I.feedPosts.filter((p) => p.web10 === web10)
+    }
+    I.getContact = function (web10) {
         const cMap = {};
-        I.contacts.map((c)=>cMap[c.web10]=c);
+        I.contacts.map((c) => cMap[c.web10] = c);
         return cMap[web10];
     }
 
-    I.isMe = function(web10) {
+    I.isMe = function (web10) {
         return web10 === I.identity.web10
     }
 
 
-    I.savePostChanges = function(draftPost){
-        I.setWallPosts(I.wallPosts.map(p=>draftPost.id===p.id?draftPost:p))
-        I.setFeedPosts(I.feedPosts.map(p=>draftPost.id===p.id?draftPost:p))
+    I.savePostChanges = function (draftPost) {
+        I.setWallPosts(I.wallPosts.map(p => draftPost.id === p.id ? draftPost : p))
+        I.setFeedPosts(I.feedPosts.map(p => draftPost.id === p.id ? draftPost : p))
     }
-    I.deletePost = function(id){
-        I.setWallPosts(I.wallPosts.filter(p=>id!==p.id))
-        I.setFeedPosts(I.feedPosts.filter(p=>id!==p.id))
+    I.deletePost = function (id) {
+        I.setWallPosts(I.wallPosts.filter(p => id !== p.id))
+        I.setFeedPosts(I.feedPosts.filter(p => id !== p.id))
     }
-    I.createPost = function(draftPost){
+    I.createPost = function (draftPost) {
         // for now, randomly generate an id...
-        const randIDDraftPost = {...draftPost,id:Math.random(1000000000000000)}
+        const randIDDraftPost = { ...draftPost, id: Math.random(1000000000000000) }
         I.setWallPosts([randIDDraftPost].concat(I.wallPosts));
         I.setFeedPosts([randIDDraftPost].concat(I.feedPosts));
     }
 
-    I.deleteCurrentContact = function(){
-        I.setContacts(I.contacts.filter((c)=>c.id!==I.currentContact.id));
+    I.deleteCurrentContact = function () {
+        I.setContacts(I.contacts.filter((c) => c.id !== I.currentContact.id));
         I.setMode("contacts");
     }
 
-    I.cancelIdentityChanges = function(){
+    I.cancelIdentityChanges = function () {
         I.setDraftIdentity(I.identity);
     }
-    I.saveIdentityChanges = function(){
+    I.saveIdentityChanges = function () {
         I.setIdentity(I.draftIdentity);
     }
 
-    I.deleteBulletin = function(id){
-        I.setBulletin(I.bulletin.filter((b)=>b.id!==id));
+    I.deleteBulletin = function (id) {
+        I.setBulletin(I.bulletin.filter((b) => b.id !== id));
     }
 
-    I.chat = function(web10){
+    I.chat = function (web10) {
         I.setCurrentContact(I.getContact(web10))
         //I.setCurrentMessages
         I.setMode("chat");
     }
 
-    I.selectMessage= function(id){
-        I.setSelectedMessages(I.currentMessages.filter((m)=>m.id===id).concat(I.selectedMessages))
+    I.selectMessage = function (id) {
+        I.setSelectedMessages(I.currentMessages.filter((m) => m.id === id).concat(I.selectedMessages))
     }
-    I.deSelectMessage = function(id){
-        I.setSelectedMessages(I.selectedMessages.filter((m)=>m.id!==id))
+    I.deSelectMessage = function (id) {
+        I.setSelectedMessages(I.selectedMessages.filter((m) => m.id !== id))
     }
-    I.deleteSelectedMessages = function(){
-        const current = I.currentMessages.filter((m)=>!I.selectedMessages.includes(m))
+    I.deleteSelectedMessages = function () {
+        const current = I.currentMessages.filter((m) => !I.selectedMessages.includes(m))
         I.setCurrentMessages(current);
         I.setSelectedMessages([]);
     }
-    I.resetSelectedMessages = function(){
+    I.resetSelectedMessages = function () {
         I.setSelectedMessages([]);
     }
 
 
-    I.setMode = function(mode){
+    I.setMode = function (mode) {
         I.setMenuCollapsed(true);
         I.setSearch("")
         I._setMode(mode);
@@ -113,7 +142,7 @@ function useInterface() {
     }
 
     I.toggleTheme = function () {
-        I.theme=="dark"?I.setTheme("light"):I.setTheme("dark")
+        I.theme == "dark" ? I.setTheme("light") : I.setTheme("dark")
     }
 
     return I;
