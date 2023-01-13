fbc1a821277b0d45e90b1e84c746aa18e5edf08b
jacoby149 <jackstudent149@gmail.com>
2023-01-13 16:27:43 -0500

    built out a broken adapter lol

--- src/interfaces/Web10SocialAdapter.js
+++ src/interfaces/Web10SocialAdapter.js
@@ -51,59 +51,82 @@ function InstallWeb10SocialAdapter(I) {
     ];
     I.wapi.SMROnReady(sirs, []);
 
-    //TODO implement contact functions
     I.socialAdapter.loadContacts = function () {
-        return
+        return wapi.read("contacts")
     }
-    I.socialAdapter.AddContact = function () {
-        return
+    I.socialAdapter.AddContact = function (username, provider) {
+        return wapi
+            .create("contacts", {
+                username: username,
+                provider: provider,
+                date_added: new Date(),
+            })
+            .then(loadNotes);
     }
-    I.socialAdapter.deleteContacts = function () {
-        return
+    I.socialAdapter.deleteContacts = function (contactID) {
+        return wapi.delete("contacts", { id: contactID })
     }
 
-    //TODO implement Identity functions
-    I.socialAdapter.initializeIdentity = function () {
-        return
-    }
     I.socialAdapter.loadIdentity = function () {
-        return
+        return wapi.read("identity")
     }
-    I.socialAdapter.editIdentity = function () {
-        return
+    I.socialAdapter.editIdentity = function (pic, name, bio) {
+        return wapi.update("identity", {}, { pic: pic, name: name, bio: bio }).catch((error) => {
+            //TODO if an update_one fails BECAUSE there are NO records, create a record...
+            if (error.detail === "TODO DO THIS!!!") {
+                wapi.create("identity", { bio: "add a bio!" })
+            }
+        })
     }
 
-    //TODO implement messaging functions
-    I.socialAdapter.CreateMessage = function () {
-        return
+    I.socialAdapter.CreateMessage = function (message) {
+        return wapi.create("messages", {
+            message: messages,
+            sentTime: new Date(),
+            provider: wapi.readToken().provider,
+            web10: wapi.readToken().username,
+        })
     }
     I.socialAdapter.loadMessages = function () {
-        return
+        return wapi.read("messages");
     }
-    I.socialAdapter.deleteMessages = function () {
-        return
+    I.socialAdapter.deleteMessages = function (ids) {
+        const responses = ids.map((id) => {
+            return wapi.delete("messages", { id: id })
+            //TODO add a .catch...
+        })
+        return responses
     }
 
     //TODO implement post functions
-    I.socialAdapter.createPost = function () {
-        return
+    I.socialAdapter.createPost = function (html, media) {
+        return wapi.create("posts", {
+            html: html,
+            media: media,
+            time: new Date(),
+            provider: wapi.readToken().provider,
+            username: wapi.readToken().username,
+        })
     }
     I.socialAdapter.loadPosts = function () {
-        return
+        return wapi.read("posts");
     }
-    I.socialAdapter.editPost = function () {
-        return
+    I.socialAdapter.editPost = function (html, media) {
+        wapi.update("posts", {
+            html: html,
+            media: media,
+        })
     }
-    I.socialAdapter.deletePost = function () {
-        return
+    I.socialAdapter.deletePost = function (id) {
+        return wapi.delete("posts", { id: id })
     }
 
     //TODO implement bulletin functions
     I.socialAdapter.loadBulletins = function () {
-        return
+        return wapi.read("bulletin");
     }
-    I.socialAdapter.deleteBulletin = function () {
-        return
+    I.socialAdapter.deleteBulletin = function (id) {
+        return wapi.delete("bulletin", { id: id })
     }
 }
 
